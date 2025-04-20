c614c0bace14850d2f90224e60122a7f188538ad
mneedham01 <mneedham@smith.edu>
2025-04-20 01:53:57 -0400

    added recent works section! (messed up centering)

---
+++ featured/src.js
@@ -0,0 +1,67 @@
+function change_color(id) {
+    // if you are already on the page, don't highlight pink on mouse over 
+    let elem  = document.getElementById(id);
+    // need to change the photo for this one 
+    if (id=="emulate"){
+        elem.src = "../images/emulate/emulate_pink.png";
+    } else {
+        elem.style.color = "HotPink";
+    }
+}
+
+function change_back(id) {
+    let elem  = document.getElementById(id);
+    // need to change the photo for this one 
+    if (id=="emulate"){
+        elem.src = "../images/emulate/emulate_white.png";
+    } else {
+        elem.style.color = "white";
+    }
+}
+
+var pages = 3; 
+var img = document.getElementById("img");
+
+// shows the previous img 
+function prev() {
+    // first, get the current img # 
+    var num = getNum();
+
+    // subtract 1. If you get 0, go to num of pages 
+    var newNum = ( num - 1 == 0 ) ? pages : (num - 1); 
+
+    // replace the src of the image 
+    img.src = "../images/featured/liminal-smith/" + newNum + ".jpg";
+
+    changeCaption(newNum);
+}
+// shows the next img 
+function next() {
+    // first, get the current img # 
+    var num = getNum();
+
+    // add 1. If you get the num of pages + 1, then go to 1 
+    var newNum = ( num + 1 == pages + 1 ) ? 1 : (num + 1); 
+
+    // replace the src of the image 
+    img.src = "../images/featured/liminal-smith/" + newNum + ".jpg";
+
+    changeCaption(newNum);
+}
+
+// extracts the number from the src 
+function getNum() {
+    // first, get the current img # 
+    var path = img.getAttribute('src');
+    var match = path.match(/\/(\d+)\.jpg$/);
+    var number = match[1]; // "1" as a string
+    var numAsInt = parseInt(number, 10); // as an integer
+    
+    return numAsInt;
+}
+
+// replaces the caption (1/3, 2/3, etc)
+function changeCaption(num) {
+    var caption = document.getElementById("caption");
+    caption.textContent = num + " / " + pages;
+}
