b49e05ab42b16b93f5d73960da806f1cb597931a
mneedham01 <mneedham@smith.edu>
2025-04-19 22:38:58 -0400

    changed folder names to omit spaces

--- load/src.js
+++ load/src.js
@@ -42,5 +42,5 @@ window.addEventListener("load", call_random_rotate);
 window.addEventListener("click", enterPage);
 
 function enterPage(){
-    window.location.href = "home page/main.html";
+    window.location.href = "home/main.html";
 }
