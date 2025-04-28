32467a493415205b1d4e0e8d7f2a20bd8949cbcf
mneedham01 <mneedham@smith.edu>
2025-04-27 21:10:13 -0400

    commented code

--- load/src.js
+++ load/src.js
@@ -1,32 +1,34 @@
-// first, sets up storage for the letters 
-const letterIds = {};
+// setting up ids + letter lists 
 const ids = ["e_image", "m_image", "u_image", "l_image", "a_image", "t_image", "i_image", "n_image", "g_image"];
 const letters = ["E", "M", "U", "L", "A", "T", "I", "N", "G"];
 
 // first, see if the viewer is on a small screen 
 const mediaQuery = window.matchMedia('(max-width: 750px)');
+// handle the first check 
+handleMediaQueryChange(mediaQuery);
+// Continually call handleMediaQueryChange if it goes above/below 750px
+mediaQuery.addEventListener('change', handleMediaQueryChange);
 
+// changes behavior of screen -- if on a phone, does not 
+// have onmouseover component and does not rotate the subtitles
 function handleMediaQueryChange (e) {
     const emulating = document.getElementById("emulating");
     if (e.matches) {
         // then, rewrite the on mouse over function to nothing
         emulating.onmouseover = null;
-        // then, only randomly rotate the letters 10 degrees 
+        // then, only randomly rotate the letters 10 degrees + call random letters
         // we do not randomly rotate the subtitles
         for (let i = 0; i < ids.length; i++) {
             random_rotate(ids[i], 10);
             random_letter(ids[i],letters[i]);
         }
     } else {
+        // if on a laptop, need to use this to call random rotate on load 
         window.addEventListener("load", call_random_rotate);
     }
 }
 
-// Initial check
-handleMediaQueryChange(mediaQuery);
-// Continually call handleMediaQueryChange if it goes above/below 750px
-mediaQuery.addEventListener('change', handleMediaQueryChange);
-
+// onmouseover function for entire window
 function call_random_rotate() {
     for (let i = 0; i < ids.length; i++) {
         random_rotate(ids[i], 15);
@@ -38,6 +40,7 @@ function call_random_rotate() {
     }
 }
 
+// rotates randomly within a give n degree range 
 function random_rotate(id, degree){
     // chooses between -15 and 15 degrees 
     const min = degree * -1 ;
@@ -47,19 +50,20 @@ function random_rotate(id, degree){
     document.getElementById(id).style.transform = "rotate("+ toRotate.toString() + "deg)";
 }
 
+// changes the letters randomly 
 function random_letter(id, letter){
     const min = 1;
     const max = 11;
     let currentImg = Math.floor(Math.random() * (max - min + 1)) + min;
-    // sets the most current id for a given letter so it can be accessed in other pages 
-    letterIds[letter] = currentImg;
 
     let img = document.getElementById(id);
     img.src = "images/letters/" + letter + "/" + currentImg + ".png";
 }
 
+// if you click anywhere within the window, you will enter main page 
 window.addEventListener("click", enterPage);
 
+// redirects to main page
 function enterPage(){
     window.location.href = "home/main.html";
 }
