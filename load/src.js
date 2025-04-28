540e96e0b2c7c4b655df67cffc2f07cf3d0c5244
mneedham01 <mneedham@smith.edu>
2025-04-27 21:02:27 -0400

    made it more mobile responsive -- only does onmouse over on laptop

--- load/src.js
+++ load/src.js
@@ -1,30 +1,50 @@
-// initiates an object to hold the ids of the current letters to pass to other pages 
+// first, sets up storage for the letters 
 const letterIds = {};
+const ids = ["e_image", "m_image", "u_image", "l_image", "a_image", "t_image", "i_image", "n_image", "g_image"];
+const letters = ["E", "M", "U", "L", "A", "T", "I", "N", "G"];
 
-// setInterval(call_random_rotate, 1000);
-function call_random_rotate() {
-    let ids = ["e_image", "m_image", "u_image", "l_image", "a_image", "t_image", "i_image", "n_image", "g_image"]
-    let letters = ["E", "M", "U", "L", "A", "T", "I", "N", "G"]
-    for (let i = 0; i < ids.length; i++) {
-        random_rotate(ids[i]);
-        random_letter(ids[i],letters[i] );
+// first, see if the viewer is on a small screen 
+const mediaQuery = window.matchMedia('(max-width: 750px)');
+
+function handleMediaQueryChange (e) {
+    const emulating = document.getElementById("emulating");
+    if (e.matches) {
+        // then, rewrite the on mouse over function to nothing
+        emulating.onmouseover = null;
+        // then, only randomly rotate the letters 10 degrees 
+        // we do not randomly rotate the subtitles
+        for (let i = 0; i < ids.length; i++) {
+            random_rotate(ids[i], 10);
+            random_letter(ids[i],letters[i]);
+        }
+    } else {
+        window.addEventListener("load", call_random_rotate);
     }
+}
 
+// Initial check
+handleMediaQueryChange(mediaQuery);
+// Continually call handleMediaQueryChange if it goes above/below 750px
+mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+function call_random_rotate() {
+    for (let i = 0; i < ids.length; i++) {
+        random_rotate(ids[i], 15);
+        random_letter(ids[i],letters[i]);
+    }
     let subtitles = ["click_image", "anywhere_image", "to_image", "enter_image"];
     for(let i = 0; i < subtitles.length; i++){
-        random_rotate(subtitles[i]);
+        random_rotate(subtitles[i], 15);
     }
-
-    // puts letterIds object in local storage so most recent information can be accessed by other pages 
-    localStorage.setItem("letterIds", letterIds);
 }
-function random_rotate(id){
-    // chooses between -30 and 30 degrees 
-    const min = -15;
-    const max = 15;
-    var degree = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    document.getElementById(id).style.transform = "rotate("+ degree.toString() + "deg)";
+function random_rotate(id, degree){
+    // chooses between -15 and 15 degrees 
+    const min = degree * -1 ;
+    const max = degree;
+    var toRotate = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    document.getElementById(id).style.transform = "rotate("+ toRotate.toString() + "deg)";
 }
 
 function random_letter(id, letter){
@@ -38,7 +58,6 @@ function random_letter(id, letter){
     img.src = "images/letters/" + letter + "/" + currentImg + ".png";
 }
 
-window.addEventListener("load", call_random_rotate);
 window.addEventListener("click", enterPage);
 
 function enterPage(){
